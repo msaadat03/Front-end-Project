d7435cdb20129e055c128b6055d89482726b49db
msaadat03 <saadatam.code.edu.az>
2022-09-12 18:54:36 +0300

    Swiper-Slider

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,3 @@
-window.onscroll = function() {scrollFunction()};
-
-   function scrollFunction() {
-     if (document.body.scrollTop > 195|| document.documentElement.scrollTop > 195) {
-        scrollSection.style.top = "0";
-     } else {
-        scrollSection.style.top = "-62px";
-        $("div").removeClass("inActive");
-     }
-   }
-
+let swiper = new Swiper(".mySwiper",{
+    loop:true,
+})
