ad42e1bd538dc53107c40ad0f903ef96eb89be32
msaadat03 <saadatam.code.edu.az>
2022-09-19 02:16:14 +0300

    Home,contact,register

--- assets/js/home.js
+++ assets/js/home.js
@@ -20,6 +20,50 @@ let swiper = new Swiper(".mySwiper", {
 
 
 
+
+
+function openTabMenu(evt, TabMenu) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(TabMenu).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+
+  var swiper1 = new Swiper("#tab-menu .mySwiper1", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+  });
+  
+  
+  
+  
+  
+  var swiper3 = new Swiper("#tab-menu .mySwiper3", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+  });
+  
+  var swiper4 = new Swiper("#tab-menu .mySwiper4", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+  });
+
+/////
+
+
 {
 
     const MathUtil = {
